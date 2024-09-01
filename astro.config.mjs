cd32b031a5156c3408e52e0e7c5eb5c6355f273a
Andres Martin Angulo <monkeyandres@protonmail.com>
2024-09-01 20:20:35 +0200

    fix: rendering of Katex formulas

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,21 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import pagefind from "astro-pagefind";
 
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://monkeyandres.com",
-  integrations: [tailwind(), sitemap(), mdx(), pagefind()],
+  integrations: [
+    tailwind(),
+    sitemap(),
+    mdx({
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeKatex],
+    }),
+    pagefind(),
+  ],
   markdown: {
     shikiConfig: {
       theme: "css-variables",
