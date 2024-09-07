683aab2c292df57df5a73e4ac4b213843fa6ac5a
Andres Martin Angulo <monkeyandres@protonmail.com>
2024-09-07 22:55:08 +0200

    feat: add node SSR adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,32 +1,30 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import pagefind from "astro-pagefind";
-
 import rehypeKatex from "rehype-katex";
 import remarkMath from "remark-math";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
   site: "https://monkeyandres.com",
-  integrations: [
-    tailwind(),
-    sitemap(),
-    mdx({
-      remarkPlugins: [remarkMath],
-      rehypePlugins: [rehypeKatex],
-    }),
-    pagefind(),
-  ],
+  integrations: [tailwind(), sitemap(), mdx({
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex]
+  }), pagefind()],
   markdown: {
     shikiConfig: {
-      theme: "css-variables",
-    },
+      theme: "css-variables"
+    }
   },
   redirects: {
-    "/about": "/",
+    "/about": "/"
   },
+  adapter: node({
+    mode: "standalone"
+  })
 });
