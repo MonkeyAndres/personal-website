315cae780d12ef10d4804196682c38572355b059
Andres Martin Angulo <monkeyandres@protonmail.com>
2024-09-07 23:00:15 +0200

    chore: fuck Mermaid + back to SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,29 @@
-import tailwind from "@astrojs/tailwind";
-import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 import pagefind from "astro-pagefind";
+import { defineConfig } from "astro/config";
 import rehypeKatex from "rehype-katex";
 import remarkMath from "remark-math";
 
-import node from "@astrojs/node";
-
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
   site: "https://monkeyandres.com",
-  integrations: [tailwind(), sitemap(), mdx({
-    remarkPlugins: [remarkMath],
-    rehypePlugins: [rehypeKatex]
-  }), pagefind()],
+  integrations: [
+    tailwind(),
+    sitemap(),
+    mdx({
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeKatex],
+    }),
+    pagefind(),
+  ],
   markdown: {
     shikiConfig: {
-      theme: "css-variables"
-    }
+      theme: "css-variables",
+    },
   },
   redirects: {
-    "/about": "/"
+    "/about": "/",
   },
-  adapter: node({
-    mode: "standalone"
-  })
 });
