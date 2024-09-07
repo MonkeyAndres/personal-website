eca369878e4eb1b439e31c5caad0677bff9e8415
Andres Martin Angulo <monkeyandres@protonmail.com>
2024-09-07 22:44:19 +0200

    feat: publish post about "The Nature of Technology" + mermaid support

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,7 @@ import remarkMath from "remark-math";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "hybrid",
   site: "https://monkeyandres.com",
   integrations: [
     tailwind(),
