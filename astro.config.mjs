cab07428ef9d0f24b75bb4de60af3b29a85dc796
Andres Martin Angulo <monkeyandres@protonmail.com>
2024-09-28 14:22:44 +0200

    fix: remove pagefind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import pagefind from "astro-pagefind";
 import { defineConfig } from "astro/config";
 import rehypeKatex from "rehype-katex";
 import remarkMath from "remark-math";
@@ -16,7 +15,6 @@ export default defineConfig({
       remarkPlugins: [remarkMath],
       rehypePlugins: [rehypeKatex],
     }),
-    pagefind(),
   ],
   markdown: {
     shikiConfig: {
